7c1f4059bec273aeab5a0d02afa64bfc3f1c5a8a
Roisin-Fallon <48020027+Roisin-Fallon@users.noreply.github.com>
2019-04-28 23:18:50 +0100

    Changed y axis label

--- dist.py
+++ dist.py
@@ -20,16 +20,16 @@ sns.distplot(ds.loc[ds.species=='setosa', "sepal_length"] , color="dodgerblue", 
 sns.distplot(ds.loc[ds.species=='virginica', "sepal_length"] , color="orange", label="virginica")               # Plot virginica species sepal length
 sns.distplot(ds.loc[ds.species=='versicolor', "sepal_length"] , color="deeppink", label="versicolor")           # Plot versicolor species sepal length
 plt.xlabel("Sepal Length", fontweight="bold", fontsize="12", color="b")                                         # Label of the x-axis
-plt.ylabel("Frequency", fontweight="bold", fontsize="12", color="b")                                            # Label of the y-axis
-plt.title("Iris Sepal Length Histogram", fontweight="bold", fontsize="10", color="r") 
+plt.ylabel("Count", fontweight="bold", fontsize="12", color="b")                                                # Label of the y-axis
+plt.title("Iris Sepal Length Histogram", fontweight="bold", fontsize="10", color="r")                           # Label title of distplot
 
 plt.subplot(2,2,2)                                                                                              # Makes a top right subplot active in a 2x2 subplot layout
 sns.distplot( ds.loc[ds.species=='setosa', "sepal_width"] , color="dodgerblue", label="setosa")                 # Plot setosa species sepal width
 sns.distplot( ds.loc[ds.species=='virginica', "sepal_width"] , color="orange", label="virginica")               # Plot virginica species sepal width
 sns.distplot( ds.loc[ds.species=='versicolor', "sepal_width"] , color="deeppink", label="versicolor")           # Plot versicolor species sepal width
 plt.xlabel("Sepal Width", fontweight="bold", fontsize="12", color="b")                                          # Label of the x-axis
-plt.ylabel("Frequency", fontweight="bold", fontsize="12", color="b")                                            # Label of the y-axis
-plt.title('Iris Sepal Width Histogram', fontweight="bold", fontsize="10", color="r")
+plt.ylabel("Count", fontweight="bold", fontsize="12", color="b")                                                # Label of the y-axis
+plt.title('Iris Sepal Width Histogram', fontweight="bold", fontsize="10", color="r")                            # Label title of distplot
 
 
 plt.subplot(2,2,3)                                                                                              # Makes a bottom left subplot active in a 2x2 subplot layout
@@ -37,7 +37,7 @@ sns.distplot( ds.loc[ds.species=='setosa', "petal_length"] , color="dodgerblue",
 sns.distplot( ds.loc[ds.species=='virginica', "petal_length"] , color="orange", label="virginica")              # Plot virginica species petal length
 sns.distplot( ds.loc[ds.species=='versicolor', "petal_length"] , color="deeppink", label="versicolor")          # Plot versicolor species petal length
 plt.xlabel("Petal Length", fontweight="bold", fontsize="12", color="b")                                         # Label of the x-axis
-plt.ylabel('Frequency', fontweight="bold", fontsize="12", color="b")                                            # Label of the y-axis
+plt.ylabel("Count", fontweight="bold", fontsize="12", color="b")                                                # Label of the y-axis
 plt.title('Iris Petal Length Histogram', fontweight="bold", fontsize="10", color="r")                           # Label title of distplot
 
 
@@ -46,10 +46,10 @@ sns.distplot( ds.loc[ds.species=='setosa', "petal_width"] , color="dodgerblue", 
 sns.distplot( ds.loc[ds.species=='virginica', "petal_width"] , color="orange", label="virginica")              # Plot virginica species petal width
 sns.distplot( ds.loc[ds.species=='versicolor', "petal_width"] , color="deeppink", label="versicolor")          # Plot versicolor species petal width
 plt.xlabel("Petal Width", fontweight="bold", fontsize="12", color="b")                                         # Label of the x-axis
-plt.ylabel('Frequency', fontweight="bold", fontsize="12", color="b")                                           # Label of the y-axis
+plt.ylabel("Count", fontweight="bold", fontsize="12", color="b")                                                # Label of the y-axis
 plt.title('Iris Petal Width Histogram', fontweight="bold", fontsize="10", color="r")                           # Label title of distplot
 
-plt.legend(bbox_to_anchor=(1,2), loc=2)                                                                       # Legend for the overall plot
+plt.legend(bbox_to_anchor=(1,2), loc=2)                                                                        # Legend for the overall plot
 plt.show()                                                                                                     # Display the distplot
 
 
